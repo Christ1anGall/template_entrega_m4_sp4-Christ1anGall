70b2cc211b0379c3ffa1492303fdb64e247a10c7
Christ1anGall <christiangall92@icloud.com>
2022-10-06 22:34:30 -0300

    fix/teste-duplicate-name

--- src/services/categories/createCategory.service.js
+++ src/services/categories/createCategory.service.js
@@ -2,6 +2,15 @@ import database from "../../database";
 
 const createCategoryService = async (nome) => {
   try {
+    const nameExist = await database.query(
+      "SELECT name FROM categories WHERE name LIKE $1 ",
+      [nome]
+    );
+
+    if (nameExist.rows[0]?.name === nome) {
+      throw new Error("Name Alredy Exist");
+    }
+
     const res = await database.query(
       "INSERT INTO Categories(name) VALUES($1) RETURNING *",
       [nome]
